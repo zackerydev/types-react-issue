058f6e4de8c9128f43e612ee75e4185d7de4facd
Zackery Griesinger <Zack>
2023-09-24 11:37:38 -0500

    Broken with an additional component

--- react-18-app/src/App.tsx
+++ react-18-app/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Disclosure } from '@headlessui/react'
+import { Disclosure, Listbox } from '@headlessui/react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -38,6 +38,17 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    <Listbox value={'test'} onChange={() => 'test'}>
+      <Listbox.Button>{'test'}</Listbox.Button>
+      <Listbox.Options>
+          <Listbox.Option
+            key={'asdf'}
+            value="Test"
+          >
+          Test
+          </Listbox.Option>
+      </Listbox.Options>
+    </Listbox>
     </>
   )
 }
